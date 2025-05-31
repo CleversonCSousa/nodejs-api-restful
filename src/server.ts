cba96d4e12e89a8b2759b4cd449e6146641457ed
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-05-31 15:44:26 -0300

    Utilização de cookies para gerenciar a sessão do usuário

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import './env';
 import fastify from 'fastify';
 import { env } from './env';
 import { transactionsRoutes } from './routes/transactions';
+import fastifyCookie from '@fastify/cookie';
 
 const app = fastify();
 
+app.register(fastifyCookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions'
 });
