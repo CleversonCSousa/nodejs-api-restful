16517b2f3bc3738112a6fcf11b93e7ec4b0c205a
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-06-01 11:35:27 -0300

    Criado validação da existência de cookie e alteração nas querys para buscar apenas transações individuais baseadas no cookie session-id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,17 +2,29 @@ import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { knex } from '../database';
 import { randomUUID } from 'crypto';
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select();
+  app.get('/', {
+    preHandler: [
+      checkSessionIdExists
+    ]
+  }, async (request, reply) => {
+    const { sessionId } = request.cookies;
+
+    const transactions = await knex('transactions').where('session_id', sessionId).select();
 
     return {
       transactions
     };
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', {
+    preHandler: [
+      checkSessionIdExists
+    ]
+  }, async (request) => {
+    const { sessionId } = request.cookies;
 
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
@@ -20,14 +32,28 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     const { id } = getTransactionParamsSchema.parse(request.params);
 
-    const transaction = await knex('transactions').where('id', id).first()
+    const transaction = await knex('transactions')
+      .where({
+        session_id: sessionId,
+        id
+      })
+      .first()
     return { 
       transaction 
     };
   })
 
-  app.get('/summary', async () => {
-    const summary = await knex('transactions').sum('amount', { as: 'amount' }).first();
+  app.get('/summary',  {
+    preHandler: [
+      checkSessionIdExists
+    ]
+  }, async (request) => {
+    const { sessionId } = request.cookies;
+
+    const summary = await knex('transactions')
+      .where('session_id', sessionId)
+      .sum('amount', { as: 'amount' })
+      .first();
 
     return {
       summary
@@ -50,7 +76,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
       reply.cookie('sessionId', sessionId, {
         path: '/',
         maxAge: 60 * 60 * 24 * 7 // 7 days
-      })
+      });
     }
 
     await knex('transactions').insert({
@@ -61,5 +87,5 @@ export async function transactionsRoutes(app: FastifyInstance) {
     })
 
     return reply.status(201).send();
-  });
+  })
 }
