43ef7f37a96046b61eee46914f81753f8efda10e
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-06-02 13:14:24 -0300

    Testando a rota de criação de transação

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,11 +1,22 @@
-import { expect, test } from 'vitest';
+import { test, beforeAll, afterAll } from 'vitest';
+import request from 'supertest';
+import { app } from '../src/app';
 
-test('o usuário consegue criar uma nova transação', () => {
-  // fazer a chamada HTTP p/criar uma nova transação
+beforeAll(async () => {
+  await app.ready();
+});
 
-  // validação
+afterAll(async () => {
+  await app.close();
+});
 
-  const responseStatusCode = 201;
-
-  expect(responseStatusCode).toEqual(500);
+test('user can create a new transaction', async () => {
+  const response = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201);
 });
